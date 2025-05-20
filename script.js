ad515f10a647bf07ba64b9b386960b027d13efc8
mosfi99 <mosfi99.mosfi99@gmail.com>
2025-05-20 13:15:28 +0200

    Add a mess

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const elements = {
 	previousDisplay: document.querySelector('.previous-display'),
 	currentDisplay: document.querySelector('.current-display'),
 	operatorDisplay: document.querySelector('.operator-display'),
+	resultDisplay: document.querySelector('.result'),
 	numBtns: document.querySelectorAll('.btn-number'),
 	operatorBtns: document.querySelectorAll('.btn-operator'),
 	clearBtn: document.querySelector('.btn-clear'),
@@ -15,47 +16,88 @@ const calculator = {
 	currentInput: '',
 	previousInput: '',
 	operator: '',
+	hasFirstInput: false,
+	hasSecondInput: false,
+	result: '',
 };
+
+// Operation variables
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
 
 // Display functions
 function updateDisplay() {
 	elements.previousDisplay.textContent = calculator.previousInput;
 	elements.operatorDisplay.textContent = calculator.operator;
 	elements.currentDisplay.textContent = calculator.currentInput;
+	elements.resultDisplay.textContent = calculator.result;
 }
 
 function appendNumber(num) {
 	calculator.currentInput += num; // User enters digits
 	updateDisplay();
 }
+
 function setOperator(symbol) {
-	// If current is not empty AND previous is not set,
+	// If current is not empty AND second is not set
 	if (calculator.currentInput !== '' && calculator.previousInput === '') {
-		calculator.operator = symbol; // operator can be stored and set.
-		calculator.previousInput = calculator.currentInput; // previous stores current.
-		calculator.currentInput = ''; // current reset for next input.
+		// operator can be stored and set.
+		calculator.operator = symbol;
+		// previous stores first number.
+		calculator.previousInput = calculator.currentInput;
+		// current is reset for next input.
+		calculator.currentInput = '';
+		// First number is set
+		firstNumber = Number(calculator.previousInput);
+		calculator.hasFirstInput = true;
+		state(); // debugging
 	}
-	// TODO:
-	// If the numbers are set for bot (not empty)
-	if (calculator.currentInput !== '' && calculator.previousInput !== '') {
+	// If first number is set and there is an operator, but the next number is not set yet:
+	if (calculator.operator && calculator.currentInput === '') {
+		calculator.operator = symbol; // change the operator.
+	}
+	updateDisplay();
+}
+
+function getResult() {
+	// maybe i could join this if with the next one just by checking:  calculator.currentInput !== '' and there is no need fot the second input boolean
+	if (calculator.hasFirstInput && calculator.currentInput !== '') {
+		secondNumber = Number(calculator.currentInput);
+		calculator.hasSecondInput = true;
+	}
+
+	if (calculator.hasFirstInput && calculator.hasSecondInput) {
+		operator = calculator.operator;
 		// next time an operator is clicked it should:
 		// first, evaluate the initial pair of numbers (12 + 7),
+		const result = operate(firstNumber, operator, secondNumber);
 		// then display the result of that calculation (19).
+		calculator.result = `= ${result}`;
+		// TODO:
 		// Finally, use that result (19) as the first number in a new calculation, along with the next operator (-).
 	}
 	updateDisplay();
 }
 
+// still need to make decimal functionality
+
 function deleteLastDigit() {
-	calculator.currentInput = calculator.currentInput.slice(0, -1);
-	updateDisplay();
+	if (!calculator.result){
+		calculator.currentInput = calculator.currentInput.slice(0, -1);
+		updateDisplay();
+	}
 }
 
 function clearDisplay() {
 	calculator.currentInput = '';
 	calculator.previousInput = '';
 	calculator.operator = '';
+	calculator.result = '';
+	firstNumber = null;
+	secondNumber = null;
 	updateDisplay();
+	state();
 }
 
 // Event listeners
@@ -64,15 +106,16 @@ function initEventListeners() {
 	elements.numBtns.forEach((btn) => {
 		btn.addEventListener('click', () => appendNumber(btn.textContent));
 	});
-	// clear button
-	elements.clearBtn.addEventListener('click', clearDisplay);
-	// delete button
-	elements.delBtn.addEventListener('click', deleteLastDigit);
-
 	// select operator
 	elements.operatorBtns.forEach((btn) => {
 		btn.addEventListener('click', () => setOperator(btn.textContent));
 	});
+	// clear button
+	elements.clearBtn.addEventListener('click', clearDisplay);
+	// delete button
+	elements.delBtn.addEventListener('click', deleteLastDigit);
+	// equals button
+	elements.equalsBtn.addEventListener('click', getResult);
 }
 
 // Calculation Logic
@@ -81,22 +124,18 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let firstNumber = null;
-let operator = null;
-let secondNumber = null;
-
 const operate = (a, operator, b) => {
 	switch (operator) {
 		case '+':
 			return add(a, b);
 		case '-':
 			return subtract(a, b);
-		case '/':
+		case '÷':
 			return divide(a, b);
-		case '*':
+		case '×':
 			return multiply(a, b);
 		default:
-			return 'OPERATE_ERROR';
+			return 'ERROR';
 	}
 };
 
@@ -107,3 +146,9 @@ function init() {
 }
 
 init();
+
+function state() {
+	console.log('pre', firstNumber);
+	console.log('op', operator);
+	console.log('cu', secondNumber);
+}
